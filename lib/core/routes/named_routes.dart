f287272bd9e2a20f845d292fa6ad73de385bc8a1
giselleShyp <alzawahragiselle@yahoo.com>
2026-02-14 21:57:10 +0300

    "feat: Add Home screen to routes"

--- lib/core/routes/named_routes.dart
+++ lib/core/routes/named_routes.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:resto_chain_app/core/routes/routes_names.dart';
 import 'package:resto_chain_app/features/auth/login/views/login_screen.dart';
 import 'package:resto_chain_app/features/auth/register/views/register_screen.dart';
+import 'package:resto_chain_app/features/home/views/home_screen.dart';
 import 'package:resto_chain_app/features/splash/splash_screen.dart';
 
 // Pair routes' names with the corresponding pages
@@ -17,5 +18,9 @@ List<GetPage<dynamic>> routePages = [
   GetPage(
     name: AppRoutes.register,
     page: () => RegisterScreen(),
+  ),
+  GetPage(
+    name: AppRoutes.home,
+    page: () => HomeScreen(),
   ),
 ];
